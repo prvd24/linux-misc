eef2f8c5189ea4de7717513e5309476e84801439
Praveen <174160598+prvd24@users.noreply.github.com>
2025-06-04 14:55:06 +0530

    Create firefox_user.js

---
+++ firefox_user.js
@@ -0,0 +1,135 @@
+user_pref("app.normandy.api_url", "");
+user_pref("app.normandy.enabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.contentanalysis.default_result", 0);
+user_pref("browser.contentanalysis.enabled", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.places.speculativeConnect.enabled", false);
+user_pref("browser.startup.blankWindow", false);
+user_pref("browser.startup.preXulSkeletonUI", false);
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.urlbar.quicksuggest.enabled", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.trending.featureGate", false);
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+user_pref("captivedetect.canonicalURL", "");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("devtools.debugger.remote-enabled", false);
+user_pref("dom.disable_window_move_resize", true);
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_send_http_background_request", false);
+user_pref("extensions.enabledScopes", 5);
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 1);
+user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);
+user_pref("gfx.webrender.all", true);
+user_pref("media.memory_cache_max_size", 65536);
+user_pref("media.peerconnection.enabled", false);
+user_pref("middlemouse.paste", true);
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.DNS_HTTPS.domain", "freedns.controld.com");
+user_pref("network.connectivity-service.enabled", false);
+user_pref("network.connectivity-service.nat64-check", false);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("network.file.disable_unc_paths", true);
+user_pref("network.gio.supported-protocols", "");
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 20);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 20);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+user_pref("network.http.speculative-parallel-limit", 5);
+user_pref("network.IDN_show_punycode", true);
+user_pref("network.notify.IPv6", true);
+user_pref("network.predictor.enabled", false);
+user_pref("network.predictor.enable-prefetch", false);
+user_pref("network.prefetch-next", false);
+user_pref("network.proxy.socks_remote_dns", true);
+user_pref("network.trr.custom_uri", "https://freedns.controld.com/x-adguard");
+user_pref("network.trr.default_provider_uri", "https://freedns.controld.com/x-adguard");
+user_pref("network.trr.disable-ECS", false);
+user_pref("network.trr.mode", 3);
+user_pref("network.trr.uri", "https://freedns.controld.com/x-adguard");
+user_pref("pdfjs.disabled", false);
+user_pref("pdfjs.enableScripting", false);
+user_pref("permissions.delegation.enabled", false);
+user_pref("permissions.manager.defaultsUrl", "");
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("security.cert_pinning.enforcement_level", 2);
+user_pref("security.mixed_content.block_display_content", true);
+user_pref("security.OCSP.enabled", 1)
+user_pref("security.OCSP.require", true);
+user_pref("security.pki.crlite_mode", 2);
+user_pref("security.remote_settings.crlite_filters.enabled", true);
+user_pref("security.ssl.require_safe_negotiation", true);
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+user_pref("security.tls.enable_0rtt_data", false);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.server", "");
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("webchannel.allowObject.urlWhitelist", "");
+user_pref("network.notify.checkForProxies", false);
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.fallback.software", false);
+user_pref("gfx.webrender.software.d3d11", false);
+
+
+// user_pref("dom.serviceWorkers.enabled", false);
+// user_pref("extensions.pocket.enabled", false);
+// user_pref("devtools.inspector.enabled", false);
+// user_pref("browser.cache.disk.smart_size.enabled", false);
+// https://addons.mozilla.org/en-US/firefox/addon/nicedarktheme/
+
+
+editor.singleLine.pasteNewlines	0
+clipboard.imageAsFile.enabled	false	
+widget.windows.sync-clipboard-flush	1
+widget.windows.mica
+
+
+gfx.webrender.quality.force-subpixel-aa-where-possible	true
+
+
+
+dom.security.https_first	true	
+dom.security.https_first_add_exception_on_failiure	false	
+dom.security.https_only_mode	true	
+dom.security.https_only_mode.upgrade_local	true	
+dom.security.https_only_mode_break_upgrade_downgrade_endless_loop	false	
+dom.security.https_only_mode_error_page_user_suggestions	true	
+dom.security.https_only_mode_ever_enabled	true	
+dom.security.https_only_mode_ever_enabled_pbm	true	
+dom.security.https_only_mode_pbm	true	
+dom.security.https_only_mode_send_http_background_request	false	
+dom.security.sanitizer.enabled	true	
+services.sync.prefs.sync-seen.dom.security.https_only_mode	true	
+services.sync.prefs.sync-seen.dom.security.https_only_mode_ever_enabled	true
+
+
+
+ui.elantech_gesture_hacks.enabled	0
+editor.singleLine.pasteNewlines	0
+
